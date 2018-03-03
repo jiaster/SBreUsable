a67dea468460dddd7f4d0eef2edf544f4254cbca
grahmnic <36274445+grahmnic@users.noreply.github.com>
2018-03-03 11:26:40 -0500

    Added map function to view item. Fixed gradle support dependencies.

--- app/src/main/java/com/example/jiaweizheng/sbreusable/viewItemActivity.java
+++ app/src/main/java/com/example/jiaweizheng/sbreusable/viewItemActivity.java
@@ -1,8 +1,15 @@
 package com.example.jiaweizheng.sbreusable;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -17,20 +24,24 @@ public class viewItemActivity extends Activity {
     public TextView description;
     public TextView user;
     public TextView category;
+    String userLabel = "User: ";
+    String categoryLabel = "Category: ";
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_item);
         Intent i = getIntent();
-        viewedItem = (Item)i.getSerializableExtra("CURRENT_ITEM");
-        itemName = (TextView)findViewById(R.id.itemName);
-        description = (TextView)findViewById(R.id.itemDesc);
-        user = (TextView)findViewById(R.id.itemUser);
-        category = (TextView)findViewById(R.id.itemCategory);
+        viewedItem = (Item) i.getSerializableExtra("CURRENT_ITEM");
+        itemName = (TextView) findViewById(R.id.itemName);
+        description = (TextView) findViewById(R.id.itemDesc);
+        user = (TextView) findViewById(R.id.itemUser);
+        category = (TextView) findViewById(R.id.itemCategory);
         itemName.setText(viewedItem.name);
         description.setText(viewedItem.description);
-        user.setText(viewedItem.user);
-        category.setText(viewedItem.category);
+        userLabel += viewedItem.user;
+        user.setText(userLabel);
+        categoryLabel += viewedItem.category;
+        category.setText(categoryLabel);
     }
 
     public void mapButtonGo(View view) {
