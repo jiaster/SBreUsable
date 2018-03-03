8bcccc02b9a3db13730194cc35c3202bb3a57fa4
Jia Wei Zheng <jiasterop@gmail.com>
2018-03-03 05:39:19 -0500

    addItem reduced photo

--- app/src/main/java/com/example/jiaweizheng/sbreusable/addItemActivity.java
+++ app/src/main/java/com/example/jiaweizheng/sbreusable/addItemActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.app.Activity;
@@ -13,6 +14,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.graphics.BitmapFactory;
+
 
 /**
  * Created by Jia Wei Zheng on 3/3/2018.
@@ -22,11 +25,18 @@ public class addItemActivity extends Activity{
     private static final int CAMERA_REQUEST = 1888;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private ImageButton imageButton;
+    private String title;
+    private String description;
+    private int category;
+    private EditText titleField;
+    private EditText descriptionField;
 
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_item);
+        titleField   = (EditText)findViewById(R.id.title);
+        descriptionField   = (EditText)findViewById(R.id.description);
         this.imageButton = (ImageButton)this.findViewById(R.id.add_main_image);
         ImageButton photoButton = (ImageButton) this.findViewById(R.id.add_main_image);
         photoButton.setOnClickListener(new View.OnClickListener() {
@@ -36,12 +46,48 @@ public class addItemActivity extends Activity{
                 startActivityForResult(cameraIntent, CAMERA_REQUEST);
             }
         });
+
+    }
+
+
+    public void postButton(View view) {
+        title=titleField.getText().toString();
+        description=descriptionField.getText().toString();
+        finish();
+    }
+
+    public void setLocationButton(View view) {
+
+    }
+
+    public String getItemTitle(){
+        return title;
+    }
+    public String getItemDescription(){
+        return description;
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
-            imageButton.setImageBitmap(photo);
+
+            Bitmap reducedPhoto = getResizedBitmap(photo, 72);
+
+            imageButton.setImageBitmap(reducedPhoto);
         }
+    }
+    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = 1;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true);
     }
 }
