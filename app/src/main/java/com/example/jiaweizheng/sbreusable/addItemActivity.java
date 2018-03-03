0c9e07a792428188ce277a4d6cfb1032c3164e68
Jia Wei Zheng <jiasterop@gmail.com>
2018-03-03 04:52:22 -0500

    addItem camera working

--- app/src/main/java/com/example/jiaweizheng/sbreusable/addItemActivity.java
+++ app/src/main/java/com/example/jiaweizheng/sbreusable/addItemActivity.java
@@ -6,24 +6,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.app.Activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 /**
  * Created by Jia Wei Zheng on 3/3/2018.
  */
 
 public class addItemActivity extends Activity{
+    private static final int CAMERA_REQUEST = 1888;
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.add_item);
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    private ImageButton imageButton;
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_item);
+        this.imageButton = (ImageButton)this.findViewById(R.id.add_main_image);
+        ImageButton photoButton = (ImageButton) this.findViewById(R.id.add_main_image);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageButton.setImageBitmap(photo);
         }
     }
 }
