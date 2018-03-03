5197831244ff2981837775742e756d846e4f19a6
grahmnic <36274445+grahmnic@users.noreply.github.com>
2018-03-03 06:43:08 -0500

    Added additional fields to item. Added shadows and margins between cards.

--- app/src/main/java/com/example/jiaweizheng/sbreusable/Item.java
+++ app/src/main/java/com/example/jiaweizheng/sbreusable/Item.java
@@ -4,15 +4,19 @@ class Item {
     String name;
     String description;
     int photoId;
+    int bigPhotoId;
+    String category;
     String user;
     String contact;
     float x;
     float y;
 
-    Item(String name, String description, int photoId, String user, String contact, float x, float y) {
+    Item(String name, String description, int photoId, int bigPhotoId, String category, String user, String contact, float x, float y) {
         this.name = name;
         this.description = description;
         this.photoId = photoId;
+        this.bigPhotoId = bigPhotoId;
+        this.category = category;
         this.user = user;
         this.contact = contact;
         this.x = x;
